0aa46c6ff462f1ba724411c35dcc32e188b84a8b
supunUOM <supunsandaruwan000000@gmail.com>
2020-05-22 13:09:43 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -5,6 +5,7 @@ if hungry === "yes":
         print("eat samosa");
         print("eat pizza");
         print("eat burger");
+        print("eat fires");
         
 else :
          print("do your homework");
